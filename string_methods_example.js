2f235b9aa05fce1acf07d03b53b4df4f9a001f1f
Rahul Sharma <raulsdirect@gmail.com>
2024-08-29 11:17:35 +0200

    Added string methods

---
+++ string_methods_example.js
@@ -0,0 +1,14 @@
+const name3 = "Rahul"
+console.log(name3.length)
+console.log(name3.toUpperCase())
+console.log(name3.slice(2))
+console.log(name3.slice(2,4))
+console.log(name3[2])
+console.log(name3.replace("Rah","Pa"))
+const name1 = "Joy"
+console.log(name3.concat(" is a friend of ", name1, " OK"))
+const name2 = "        Ihana"
+console.log(name2.trim())
+for(let i=0; i<name3.length; i++){
+    console.log(name3[i]);
+}
